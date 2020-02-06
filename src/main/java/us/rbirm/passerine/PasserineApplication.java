cf15a8abb00261b0d6ba49e449c64be7dd0497ca
Ryan Birmingham <birm@rbirm.us>
2020-02-06 10:28:25 -0500

    from spring init

---
+++ src/main/java/us/rbirm/passerine/PasserineApplication.java
@@ -0,0 +1,13 @@
+package us.rbirm.passerine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasserineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasserineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/us/rbirm/passerine/PasserineApplicationTests.java
@@ -0,0 +1,13 @@
+package us.rbirm.passerine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasserineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
